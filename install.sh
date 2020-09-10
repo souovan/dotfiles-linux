11911b15b2c35e5f9826e24faeab9a137d0e603d
souovan <souovan@protonmail.com>
2020-09-10 17:52:18 -0300

    Adicionando bashscript de instalação (WIP)

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#Instala oh-my-bash
+sh -c "$(curl -fsSL https://raw.github.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+# TODO
+# Criar um instalador automático para tema dracula gnome terminal
+# Criar um instalador automático para tema dracula vim
