106402f96a0fab27b6c9539dc6b84602057c1e69
souovan <souovan@protonmail.com>
2020-09-10 17:56:20 -0300

    Atualizando TODO bashscript

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
 # TODO
+# Criar o script para substituir os dotfiles renomeando os originais para .old
 # Criar um instalador automático para tema dracula gnome terminal
 # Criar um instalador automático para tema dracula vim
