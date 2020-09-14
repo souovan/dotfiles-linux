39a4ac18b62f3241fc11a8de0501df162ff23c64
souovan <souovan@protonmail.com>
2020-09-14 14:00:30 -0300

    Correção bashscript

--- install.sh
+++ install.sh
@@ -7,16 +7,16 @@
 # cat /etc/issue
 
 if [[ $(cat /etc/issue) == *"Ubuntu"* ]]; then
-  echo "Contém Ubuntu"
+#  echo "Contém Ubuntu"
   sudo -i apt install git && sudo -i apt install vim && sudo -i apt install yarn && sudo -i apt install dconf-cli
 elif [[ $(cat /etc/issue) == *"elementary"* ]]; then
-  echo "Contém ElementaryOS"
+#  echo "Contém ElementaryOS"
   sudo -i apt install git && sudo -i apt install vim && sudo -i apt install yarn && sudo -i apt install dconf-cli
 elif [[ $(cat /etc/issue) == *"Debian"* ]]; then
-  echo "Contém Debian"
+#  echo "Contém Debian"
   sudo -i apt install git && sudo -i apt install vim && sudo -i apt install yarn && sudo -i apt install dconf-cli
 else [[ $(cat /etc/redhat-release) == *"Fedora"* ]]
-  echo "Contém Fedora"
+#  echo "Contém Fedora"
   sudo -i dnf install git && sudo -i dnf install vim && sudo -i dnf install yarn && sudo -i dnf install dconf*
 fi
 
@@ -24,7 +24,7 @@ fi
 #sh -c "$(curl -fsSL https://raw.github.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
 if [ -e $HOME/gnome-terminal ]; then
-  echo "Dracula Theme para Gnome Terminal já está instalado"
+  echo " ~ Dracula Theme para Gnome Terminal já está instalado"
 else
   cd $HOME
   git clone https://github.com/dracula/gnome-terminal
@@ -34,7 +34,7 @@ else
 fi
 
 if [ -e ]; then
-  echo "Dracula Theme para VIM já está instalado"
+  echo " ~ Dracula Theme para VIM já está instalado"
 else
   mkdir ~/.vim/pack/themes/opt
   cd ~/.vim/pack/themes/opt
@@ -44,7 +44,7 @@ fi
 
 # Instala Fonts para Powerline
 if [ -e $HOME/fonts ]; then
-  echo "O Pacote de Fontes já está instalado"
+  echo " ~ O Pacote de Fontes já está instalado"
 else
   cd $HOME
   git clone https://github.com/powerline/fonts.git fonts
@@ -59,7 +59,7 @@ cp "Droid Sans Mono Nerd Font Complete.otf" "$font_dir/"
 
 # Copia os arquivos de configuração do VIM Vundle
 if [ -e $HOME/.vim/bundle/Vundle.vim ]; then
-  echo "O Vundle já está instalado"
+  echo " ~ O Vundle já está instalado"
 else
   cd $HOME
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -68,20 +68,20 @@ fi
 
 # Instala o NodeJS
 if [[ $(node --version) == *"v12"* ]]; then
-  echo "O NodeJS já está instalado"
+  echo " ~ O NodeJS já está instalado"
 else
-  sudo -i curl -sL install-node.now.sh/lts | bash
-  echo "Fim da instalação do NodeJS"
+  echo " !! Por favor instale o NodeJS como root com o comando 'curl -sL install-node.now.sh/lts | bash'"
+  echo " + Fim da instalação do NodeJS"
 fi
 
 if [ -e $HOME/.bashrc ]; then
-  echo "Backup do .bashrc feito e arquivo substituido"
+  echo " + Backup do .bashrc feito e arquivo substituido"
   mv $HOME/.bashrc $HOME/.bashrc.old
   cp ./.bashrc $HOME/
 fi
 
 if [ -e $HOME/.vimrc ]; then
-  echo "Backup do .vimrc feito e arquivo substituido"
+  echo " + Backup do .vimrc feito e arquivo substituido"
   mv $HOME/.vimrc $HOME/.vimrc.old
   cp ./.vimrc $HOME/
 else
@@ -89,5 +89,7 @@ else
   cp ./.vimrc $HOME/
 fi
 
-echo "Fim da instalação!!!"
+echo " ############################################"
+echo " # Dotfiles-linux instalado com sucesso !!! #"
+echo " ############################################"
 
