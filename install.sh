bd18cc12521cb18cd74601864bc0671a872ad2bc
souovan <souovan@protonmail.com>
2021-09-06 09:54:07 -0300

    Correting installation script and making a bit shellcheck compatible Resolve #2

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 # User colors, mas só se está conectado a um terminal e se ele aceitar cores
-if which tput >/dev/null 2>&1; then 
+if command -v tput >/dev/null 2>&1; then 
   ncolors=$(tput colors) 
 fi
 if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then 
-  RED="$(tput setaf 5)"
+# RED="$(tput setaf 5)"
   GREEN="$(tput setaf 2)"
   YELLOW="$(tput setaf 3)"
   BLUE="$(tput setaf 4)"
   NORMAL="$(tput sgr0)" 
 else
-  RED=""
+# RED=""
   GREEN=""
   YELLOW=""
   BLUE=""
@@ -23,29 +23,25 @@ fi
 # cat /etc/issue
 
 if [[ $(cat /etc/issue) == *"Debian"* ]]; then
+  if ! type vim &> /dev/null && ! type dconf &> /dev/null && ! type nvim &> /dev/null; then
+    su -c "apt install vim -y && apt install dconf-cli -y && apt install nvim"
+  else
+    printf "${YELLOW} ~ vim e dconf-cli e nvim já estão instalados${NORMAL}\n"
+  fi
 
+elif  [[ $(cat /etc/issue) == *"Ubuntu"* ]]; then 
+  if ! type vim &> /dev/null && ! type dconf &> /dev/null && ! type nvim &> /dev/null; then
+    sudo -i apt install vim -y && sudo -i apt install dconf-cli -y
+  else
+    printf "${YELLOW} ~ vim e dconf-cli e nvim já estão instalados${NORMAL}\n"
+  fi
 
-    if ! type vim &> /dev/null && ! type dconf &> /dev/null && ! type nvim &> /dev/null; then
-      su -c "apt install vim -y && apt install dconf-cli -y && apt install nvim"
-    else
-      printf "${YELLOW} ~ vim e dconf-cli e nvim já estão instalados${NORMAL}\n"
-    fi
-
-  elif  [[ $(cat /etc/issue) == *"Ubuntu"* ]]; then
-   
-    if ! type vim &> /dev/null && ! type dconf &> /dev/null && ! type nvim &> /dev/null; then
-      sudo -i apt install vim -y && sudo -i apt install dconf-cli -y
-    else
-      printf "${YELLOW} ~ vim e dconf-cli e nvim já estão instalados${NORMAL}\n"
-    fi
-
-  elif [[ $(cat /etc/issue) == *"elementary"* ]]; then
-
-    if ! type vim &> /dev/null && ! type nvim &> /dev/null; then
-      sudo -i apt install vim -y && sudo -i apt install dconf-cli -y && sudo -i apt install nvim -y
-    else
-      printf "${YELLOW} ~ vim e dconf-cli e nvim já estão instalados${NORMAL}\n"
-    fi
+elif [[ $(cat /etc/issue) == *"elementary"* ]]; then
+  if ! type vim &> /dev/null && ! type nvim &> /dev/null; then
+    sudo -i apt install vim -y && sudo -i apt install dconf-cli -y && sudo -i apt install nvim -y
+  else
+    printf "${YELLOW} ~ vim e dconf-cli e nvim já estão instalados${NORMAL}\n"
+  fi
 
   if [[ $(cat /etc/issue) == *"elementary OS Hera"* ]]; then
   # Handmade Dracula Theme for Olds ElementaryOS Console
@@ -64,18 +60,18 @@ if [[ $(cat /etc/issue) == *"Debian"* ]]; then
     gsettings set org.pantheon.terminal.settings follow-last-tab "true"
   fi
 
-  elif [[ $(cat /etc/redhat-release) == *"Fedora"* ]]
-    if ! type vim &> /dev/null && ! type dconf &> /dev/null && ! type nvim &> /dev/null; then
-      sudo -i dnf install vim -y && sudo -i dnf install dconf* -y && sudo -i dnf install nvim -y
-    else
-      printf "${YELLOW} ~ vim e dconf-cli e nvim já estão instalados${NORMAL}\n"
+elif [[ $(cat /etc/redhat-release) == *"Fedora"* ]]; then
+  if ! type vim &> /dev/null && ! type dconf &> /dev/null && ! type nvim &> /dev/null; then
+    sudo -i dnf install vim -y && sudo -i dnf install dconf* -y && sudo -i dnf install nvim -y
+  else
+    printf "${YELLOW} ~ vim e dconf-cli e nvim já estão instalados${NORMAL}\n"
   fi
 
-  elif [[ $(cat /etc/redhat-release) == *"Red Hat Enterprise Linux"* ]]
-    if ! type vim &> /dev/null && ! type dconf &> /dev/null && ! type nvim &> /dev/null; then
-      sudo -i dnf install vim -y && sudo -i dnf install dconf* -y && sudo -i dnf install nvim -y
-    else
-      printf "${YELLOW} ~ vim e dconf-cli e nvim já estão instalados${NORMAL}\n"
+elif [[ $(cat /etc/redhat-release) == *"Red Hat Enterprise Linux"* ]]; then
+  if ! type vim &> /dev/null && ! type dconf &> /dev/null && ! type nvim &> /dev/null; then
+    sudo -i dnf install vim -y && sudo -i dnf install dconf* -y && sudo -i dnf install nvim -y
+  else
+    printf "${YELLOW} ~ vim e dconf-cli e nvim já estão instalados${NORMAL}\n"
   fi
 
 fi
@@ -130,7 +126,7 @@ font_dir="$HOME/.local/share/fonts"
 cp "Droid Sans Mono Nerd Font Complete.otf" "$font_dir/"
 
 # Configura a fonte do terminal para Droid Sans Mono for Powerline Regular
-GNOME_TERMINAL_PROFILE=`gsettings get org.gnome.Terminal.ProfilesList default | awk -F \' '{print $2}'`
+GNOME_TERMINAL_PROFILE=$(gsettings get org.gnome.Terminal.ProfilesList default | awk -F \' '{print $2}')
 
 gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$GNOME_TERMINAL_PROFILE/ font 'Droid Sans Mono for Powerline Regular 12'
 
